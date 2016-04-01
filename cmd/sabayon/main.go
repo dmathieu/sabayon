72b4e08d43b81bff167f7aad70b6210a7d43e6d4
Damien Mathieu <damien@heroku.com>
2016-04-01 18:22:53 +0100

    don't use subcommands

--- cmd/sabayon/main.go
+++ cmd/sabayon/main.go
@@ -1,27 +1,90 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"log"
 	"os"
+	"time"
 
-	"github.com/codegangsta/cli"
-	"github.com/dmathieu/sabayon/commands"
+	"github.com/dmathieu/sabayon/certs"
+	"github.com/dmathieu/sabayon/heroku"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
+	godotenv.Load()
 
-	app := cli.NewApp()
-	app.Name = "sabayon"
-	app.Usage = "Manage Letsencrypt certificates on heroku"
-	app.Commands = []cli.Command{
-		commands.SetupCmd,
+	var requiredDay string
+	flag.StringVar(&requiredDay, "dat", "", "")
+	flag.Parse()
+	t := time.Now()
+	currentDay := t.Weekday().String()
+
+	if requiredDay != "" && requiredDay != currentDay {
+		log.Printf("cert.day_ignore required=%s current=%s", requiredDay, currentDay)
+		return
 	}
 
-	err = app.Run(os.Args)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	var domain = os.Getenv("ACME_DOMAIN")
+	var email = os.Getenv("ACME_EMAIL")
+	var token = os.Getenv("HEROKU_TOKEN")
+	var appName = os.Getenv("ACME_APP_NAME")
+
+	log.Printf("cert.create email='%s' domain='%s'", email, domain)
+
+	ce := certs.NewCert(email, domain)
+	go ce.Create()
+
+	herokuClient := heroku.NewClient(nil, token)
+
+	for {
+		select {
+		case r := <-ce.ErrChan:
+			log.Printf("%s", r)
+			return
+		case r := <-ce.ChallengeChan:
+			log.Printf("cert.validate")
+
+			err := herokuClient.SetConfigVars(appName, r.KeyAuth, r.Token)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// Wait for a few seconds so the app can restart
+			time.Sleep(5 * time.Second)
+
+			ce.ComChan <- "validate"
+		case r := <-ce.ComChan:
+			log.Printf("cert.com msg=%s", r)
+		case r := <-ce.CertChan:
+			log.Printf("cert.created")
+
+			certs, err := herokuClient.GetSSLCertificates(appName)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if len(certs) > 1 {
+				log.Fatalf("Found %d certificate. Can only update one. Nothing done.", len(certs))
+			}
+
+			if len(certs) == 0 {
+				err = herokuClient.SetSSLCertificate(appName, r.Certificate, r.PrivateKey)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				log.Printf("cert.added")
+			} else {
+				err = herokuClient.UpdateSSLCertificate(appName, certs[0].Name, r.Certificate, r.PrivateKey)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				log.Printf("cert.updated")
+			}
+
+			return
+		}
 	}
 }
